76b2ce56fbc6105b360d0cd56ec713dbfa4b0e7a
dimabranik <dmitriybranitskiy@gmail.com>
2018-12-02 15:59:28 +0200

    Changed user info storing, added endpoint for adding new record to user income

--- server/server.js
+++ server/server.js
@@ -24,16 +24,44 @@ const authCheck = jwt({
     algorithms: ['RS256']
 });
 
+user_data = {
+  nickname: 'IvanIvanov', 
+  accounts: {
+    totalMoney: 25000, 
+    income: {
+      curMonth: 1555,
+    }, 
+    expenses: {
+      curMonth: 200,
+    },
+  }
+}
+
 app.get('/api/userinfo', authCheck, (req,res) => {
   
   // check token, find user (or create), send back user-info 
   // const authorization = req.get('Authorization');
 
-  console.log("/api/userinfo request")
+  console.log("/api/userinfo request");
   // console.log(req)
 
-  res.json({nickname: 'IvanIvanov', accounts: {income: 1555, expenses: 200}});
-})
+  res.json(user_data);
+
+});
+
+app.post('/api/addIncome', authCheck, (req,res) => {
+  
+  // check token, find user (or create), send back user-info 
+  // const authorization = req.get('Authorization');
+
+  console.log("/api/addIncome request");
+  // console.log(req)
+
+  user_data.accounts.income.curMonth += req.incomeToAdd;
+
+  res.json(200);
+  
+});
 
 app.listen(3333);
 console.log('Listening on localhost:3333');
