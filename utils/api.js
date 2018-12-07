7c7a56c22f3da5e468ab8eea9f74697c326fd31b
dimabranik <dmitriybranitskiy@gmail.com>
2018-12-07 17:47:31 +0200

    Added api functions for using /income /expense endpoints

--- utils/api.js
+++ utils/api.js
@@ -3,41 +3,61 @@ import { getAccessToken, getIdToken } from './auth';
 
 const BASE_URL = 'http://localhost:3333';
 
-export { getUserInfo, getKeepAccounts, getExpenseCategories };
+export { getUserInfo, getKeepAccounts, getExpenseAccounts, postIncome, postExpense };
 
 function getUserInfo() {
   const url = `${BASE_URL}/api/userinfo`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
-  // return axios.get(url, { headers: { Authorization: `${getIdToken()}` }}).then(response => response.data);
+  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}`, id_token: `${getIdToken()}`, }}).then(response => response.data);
 }
 
 function getKeepAccounts() {
-  const url = `${BASE_URL}/api/keep_accounts`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+  const url = `${BASE_URL}/v1/accounts/keep`;
+  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}`, id_token: `${getIdToken()}`, }}).then(response => response.data);
 }
 
-function getExpenseCategories() {
-  const url = `${BASE_URL}/api/expense_categories`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+function getExpenseAccounts() {
+  const url = `${BASE_URL}/v1/accounts/expense`;
+  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}`, id_token: `${getIdToken()}`, }}).then(response => response.data);
+}
+
+function postIncome(account_name, amount) {
+  const url = `${BASE_URL}/v1/income`;
+  return axios.post(url, 
+    {     
+      headers: { Authorization: `Bearer ${getAccessToken()}`, id_token: `${getIdToken()}`, },
+      data: {
+        account_name: account_name,
+        amount: amount,
+      },
+      
+    }).then(response => response.data);
+}
+
+function postExpense(keep_account_name, expense_account_name, amount) {
+  const url = `${BASE_URL}/v1/expense`;
+  return axios.post(url, 
+    {     
+      headers: { Authorization: `Bearer ${getAccessToken()}`, id_token: `${getIdToken()}`, },
+      data: {
+        keep_account_name: keep_account_name,
+        expense_account_name: expense_account_name,
+        amount: amount,
+      },
+      
+    }).then(response => response.data);
 }
 
 
-
-
-
-
-
-//   // axios({
-//   //   url: `${BASE_URL}/api/postapi`
-//   //   method: 'post',
-//   //   data: payload
-//   // })
-//   // .then(function (response) {
-//   //     // your action after success
-//   //     console.log(response);
-//   // })
-//   // .catch(function (error) {
-//   //    // your action on error success
-//   //     console.log(error);
-//   // });
-
+  // axios({
+  //   url: `${BASE_URL}/api/postapi`
+  //   method: 'post',
+  //   data: payload
+  // })
+  // .then(function (response) {
+  //     // your action after success
+  //     console.log(response);
+  // })
+  // .catch(function (error) {
+  //    // your action on error success
+  //     console.log(error);
+  // });
